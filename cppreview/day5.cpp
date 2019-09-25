cc367b505be6837fea699dba2d6a1ac1772a94ed
Joshua <jschmidtnj@gmail.com>
2019-09-25 17:28:12 -0400

    added notes from today

---
+++ cppreview/day5.cpp
@@ -0,0 +1,14 @@
+// notes for 9/24
+
+// clock speed, ram speed is found in simple benchmark
+// cache doesn't really work for write operations or lots of operations (it's small)
+
+// in a cpu, there are transistors and cache
+// multiple cpu cores (multiple cpus in one)
+// there is a data bus (shared lines for lots of data to send over) to connect to the RAM and I/O
+// there's also an address bus for setting the address to save data to
+// 64 bit means 64 bits can be saved to one address
+// ALU = arithmetic logic unit (for adding, subtracting, etc)
+// arduinos are 8 bit
+// R0 to R15 are the fastest on the pi (it takes one clock cycle to read all of them)
+// FPGA = field programmable grid array
