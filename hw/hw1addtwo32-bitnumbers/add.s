167a231711a607085382c1926d5405ad1ea5b187
Joshua <jschmidtnj@gmail.com>
2019-09-27 11:03:06 -0400

    added correct git submodule using github classroom and added my add.s file

---
+++ hw/hw1addtwo32-bitnumbers/add.s
@@ -0,0 +1,8 @@
+; author: Joshua Schmidt
+; pledge: I pledge my honor that I have abided by the Stevens Honor System.
+
+	.global	_Z3addii
+_Z3addii:
+	; add your assembly code here. Don't forget to return from your subroutine
+	add r0 r1 r2
+	bx lr
