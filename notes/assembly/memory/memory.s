4c251e76f60aba3c069f4dc7675d8ab689b2579c
Joshua <jschmidtnj@gmail.com>
2019-11-12 17:49:52 -0500

    added notes from today's class

--- notes/assembly/memory/memory.s
+++ notes/assembly/memory/memory.s
@@ -15,6 +15,15 @@ loop:
   bne loop
   bx lr
 
+_Z9readwritePii:
+loop2:
+  ldr r2, [r0]
+  str r2, [r0]
+  add r0, #4
+  subs r1, #1
+  bne loop2
+  bx lr
+
 // load from and to register
 _Z9loadarrayPKii:
   .fnstart
@@ -22,3 +31,27 @@ _Z9loadarrayPKii:
   // r1 = length of the array
   ldr r2, [r0] // load from location r0 into register r2
   str r2, [r0] // store from register r2 into memory location [r0]
+
+// reads like this: 0 64 128 ... n
+//                    4 68 132 ... n
+//                     up to starting with 60
+// nested for loop:
+_Z8readskipPii:
+  push {r4, r5}
+  mov r3, r0
+  mov r4, r0
+  add r4, #60 // that's the pointer to the last value
+  mov r5, r1
+outerloop:
+  mov r0, r3
+  mov r1, r5
+innerloop:
+  ldr r2, [r0]
+  add r0, #64 // this might be too big for the compiler
+  subs r1, #16
+  bne innerloop
+  add r3, #4
+  cmp r3, r4
+  ble outerloop
+  pop {r4, r5}
+  bx lr
